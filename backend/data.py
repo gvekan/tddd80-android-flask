40632d4f9823698b63d5373db649897a3b10b894
Simon Sundberg <simsu451@student.liu.se>
2017-02-20 14:47:43 +0100

    Gjort tables

--- backend/data.py
+++ backend/data.py
@@ -1,38 +1,35 @@
 from backend import application
 from flask_sqlalchemy import SQLAlchemy
 import uuid
-
+)from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy(application)
-
-
-#Tables
-friendships = db.Table('friendships', db.Column('User', db.Integer, db.ForeignKey('user.anonymous_id')),
-                   db.Column('Friends', db.Integer, db.ForeignKey('user.anonymous_id')))
-
-friend_requests = db.Table('friend_requests', db.Column('Sender', db.Integer, db.ForeignKey('user.anonymous_id')),
-                    db.Column('Receiver', db.Integer, db.ForeignKey('user.anonymous_id')))
-
-day_friends = db.Table('day_friends', db.Column('User', db.Integer, db.ForeignKey('user.anonymous_id')),
-                    db.Column('Friend', db.Integer, db.ForeignKey('user.anonymous_id')))
-
-blocked = db.Table('blocked', db.Column('User', db.Integer, db.ForeignKey('user.anonymous_id'))
-                   db.Column('Blocked', db.Integer, db.ForeignKey('user.anonymous_id')))
-
-sent_messages = db.Table('sent_messages', db.Column('Message', db.Integer, db.ForeignKey('message.id')),
-                    db.Column('Receiver', db.Integer, db.ForeignKey('user.anonymous_id')))
-
 
 def initialize_db():
     db.drop_all()
     db.create_all()
 
+#Tables
+friendships = db.Table('friendships', db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                   db.Column('user_id_other', db.Integer, db.ForeignKey('user.id')))
 
-def match_users():
-    return None
+friend_requests = db.Table('friend_requests', db.Column('sender', db.Integer, db.ForeignKey('user.id')),
+                    db.Column('receiver', db.Integer, db.ForeignKey('user.id')))
+
+day_friends = db.Table('day_friends', db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                    db.Column('user_id_other', db.Integer, db.ForeignKey('user.id')))
+
+blocked_friends = db.Table('blocked', db.Column('User', db.Integer, db.ForeignKey('user.id'))
+                   db.Column('Blocked', db.Integer, db.ForeignKey('user.id')))
+
+sent_messages = db.Table('sent_messages', db.Column('message', db.Integer, db.ForeignKey('message.id')),
+                    db.Column('receiver', db.Integer, db.ForeignKey('user.id')))
+
 
 
 class User(db.Model):
-    anonymous_id = db.Column(db.Integer, unique=True, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, primary_key=True)
     first_name = db.Column(db.String)
     surname = db.Column(db.String)
     date_of_birth = db.Column(db.Integer, unique=True)
@@ -41,7 +38,40 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     pw_hash = db.Column(db.String(160))
 
-    friends = db.relationship('Friends', secondary=friendships, primaryjoin=anonymous_id==friendships.backref=db.backref('friends', lazy='dynamic'))
+    friends = db.relationship('Friends',
+                            secondary=friendships,
+                            primaryjoin=(friendships.c.user_id == id),
+                            secondaryjoin=(friendships.c.user_id_other == id),
+                            backref=db.backref('friendships', lazy='dynamic'),
+                            lazy='dynamic')
+
+    requests = db.relationship('Requests',
+                               secondary=friend_requests,
+                               primaryjoin=(friend_requests.c.sender == id),
+                               secondaryjoin=(friend_requests.c.receiver == id),
+                               backref=db.backref('friend_requests', lazy='dynamic'),
+                               lazy='dynamic')
+
+    friends_today = db.relationship('Friends today',
+                                    secondary=day_friends,
+                                    primaryjoin=(day_friends.c.user_id == id),
+                                    secondaryjoin=(day_friends.c.user_id_other == id),
+                                    backref=db.backref('day_friends', lazy='dynamic'),
+                                    lazy='dynamic')
+
+    blocked = db.relationship('Blocked friends',
+                              secondary=blocked_friends,
+                              primaryjoin=(blocked_friends.c.user_id == id),
+                              secondaryjoin=(blocked_friends.c.user_id == id),
+                              backref=db.backref('blocked_friends', lazy='dynamic'),
+                              lazy='dynamic')
+
+    messages = db.relationship('Message',
+                                secondary=sent_messages,
+                                backref=db.backref('users', lazy='dynamic'))
+
+
+
 
 
     def __init__(self, first_name, surname, date_of_birth, domicile, description, email):
@@ -54,13 +84,23 @@ class User(db.Model):
         self.email = email
 
 
+    def set_password(self, password):
+        self.pw_hash=generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.anonymous_id})
+
+
     def send_friend_request(self, requested):
         return None
 
-
-    def get_friend_requsts(self):
+    def get_friend_requests(self):
         return None
-
 
     def accept_friend_request(self, requester):
         return None
@@ -94,3 +134,13 @@ class Message(db.Model):
     def __init__(self, text):
         self.message_id = uuid.uuid4().int
         self.text = text
+
+def match_users(user_id, user_id_other):
+    user1 = User.query.filter()
+    return
+
+def add_user(name, password):
+    user = User(name, password)
+    db.session.add(user)
+    db.session.commit()
+    return 'User created'
