b1bc129d93dceb021e970d7ba0d9e0ed472e25e7
Gustav Andersson <gusan092@student.liu.se>
2017-02-20 14:46:20 +0100

    Skrev massor i server satte upp de flesta funktioner och jwt

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,26 @@
+import datetime
+
 from backend import config
+import simplekv.memory
 from flask import Flask
 
+
 application = Flask(__name__)
+application.secret_key = 'super-secret'
+
 application.config['SQLALCHEMY_DATABASE_URI'] = config.sqldb_uri
-application.config['SECRET_KEY'] = 'Gustav'
+
+# Enable and configure the JWT blacklist / token revoke. We are using
+# an in memory store for this example. In production, you should
+# use something persistent (such as redis, memcached, sqlalchemy).
+# See here for options: http://pythonhosted.org/simplekv/
+application.config['JWT_BLACKLIST_ENABLED'] = True
+application.config['JWT_BLACKLIST_STORE'] = simplekv.memory.DictStore()
+
+# Check all tokens (access and refresh) to see if they have been revoked.
+# You can alternately check only the refresh tokens here, by setting this
+# to 'refresh' instead of 'all'
+application.config['JWT_BLACKLIST_TOKEN_CHECKS'] = 'all'
+application.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=5)
 
 import backend.server
